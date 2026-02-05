06a44d0d16d60f9944f47a690d96ab34f3bcb63a
Sami Mikkola <fsami009@edu.xamk.fi>
2026-02-05 14:04:16 +0200

    Part 3 phase 1 done. Updateed entities, repositories, service and controller

--- SimpleExample.Domain/Entities/BaseEntity.cs
+++ SimpleExample.Domain/Entities/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace SimpleExample.Domain.Entities;
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public Guid Id { get;  set; } 
+    public DateTime CreatedAt { get;  set; } 
+    public DateTime UpdatedAt { get;  set; } 
 }
